141ae1d708d117dfa0df4a28bff589f9fe5e50c1
Greenstone00 <71177333+Greenstone00@users.noreply.github.com>
2022-10-22 11:06:44 +0200

    Zene ténylegesen Loopol Hangvezérlő elmenti a beállított hangerőt scenek közt, és még játék újraindításakor is hangvezérlő hozzáadva a pause menüköz pause menü stílusa egyeztetve a main menüvel

--- PocketPets/Assets/MenuFiles/OptionsMenu.cs
+++ PocketPets/Assets/MenuFiles/OptionsMenu.cs
@@ -1,16 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private TextMeshProUGUI volumeTextUI = null;
+
+    private void Start() //betölti az elõzõleg elmentett hangerõt megnyitáskor
+    {
+        LoadValues();
+    }
+
+    public void VolumeSlider(float volume)
+    {
+        volumeTextUI.text = volume.ToString("0.0" + "db");
+    }
+
+    public void SetVolumeButton()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+
+    public void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        //AudioListener.volume = volumeValue;
+        audioMixer.SetFloat("VolumeValue", volumeValue);
+    }
+
     public AudioMixer audioMixer;
 
+
+    /*
     public void SetVolume(float volume)
     {
         //az audiomixer paramétereinek ráhookolása kóddal hogy lehessen mozhatni a sliderrel
         audioMixer.SetFloat("volume", volume);
         Debug.Log(volume);
     }
+    */
 }
